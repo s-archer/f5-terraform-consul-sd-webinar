a9b1d52b5e84070c2f7cd89e7bcf3f726560ac27
S-Archer <s.archer@f5>
2020-06-03 15:36:21 +0100

    Updated to install as3 via user_data script at AMI deployment time.

--- brownfield-approach/2-as3-shared-pool/main.tf
+++ brownfield-approach/2-as3-shared-pool/main.tf
@@ -4,21 +4,6 @@ provider "bigip" {
   password = "${var.password}"
 }
 
-# download rpm
-resource "null_resource" "download_as3" {
-  provisioner "local-exec" {
-    command = "wget ${var.as3_rpm_url}"
-  }
-}
-
-# install rpm to BIG-IP
-resource "null_resource" "install_as3" {
-  provisioner "local-exec" {
-    command = "./install_as3.sh ${var.address}:${var.port} admin:${var.password} ${var.as3_rpm}"
-  }
-  depends_on = [null_resource.download_as3]
-}
-
 # deploy shared webapp-pool using as3
 resource "bigip_as3" "nginx" {
   as3_json    = "${file("nginx-pool.json")}"
--- terraform/f5.tf
+++ terraform/f5.tf
@@ -20,6 +20,10 @@ resource "aws_instance" "f5" {
   key_name                    = "${aws_key_pair.demo.key_name}"
   root_block_device { delete_on_termination = true }
 
+  provisioner "local-exec" {
+    command = "while [[ \"$(curl -skiu ${var.username}:${random_string.password.result} https://${self.public_ip}:${var.port}/mgmt/shared/appsvcs/declare | grep -Eoh \"^HTTP/1.1 204\")\" != \"HTTP/1.1 204\" ]]; do sleep 5; done"
+  }
+  
   tags = {
     Name = "${var.prefix}-f5"
     Env  = "consul"
@@ -28,9 +32,15 @@ resource "aws_instance" "f5" {
 }
 
 data "template_file" "f5_init" {
-  template = "${file("../scripts/f5.tpl")}"
+  template = "${file("../scripts/f5_onboard.tmpl")}"
 
   vars = {
     password = "${random_string.password.result}"
+    DO_URL       = "${var.DO_URL}",
+    AS3_URL      = "${var.AS3_URL}",
+    TS_URL       = "${var.TS_URL}",
+    CFE_URL      = "${var.CFE_URL}",
+    libs_dir     = "${var.libs_dir}",
+    onboard_log  = "${var.onboard_log}",
   }
 }
